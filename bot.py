feab18b0ae061801c6b1728b6490337f53b2aadc
ddanyaa <davidenko.dv@students.dvfu.ru>
2024-06-28 23:42:44 +1000

    v2

--- bot.py
+++ bot.py
@@ -4,12 +4,15 @@ import time
 import json
 import random
 import requests
+from zoneinfo import ZoneInfo
+import hmac
+import hashlib
+from dateutil import parser
+import numpy as np
 from json import dumps as dp, loads as ld
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from colorama import *
 from urllib.parse import unquote
-
-init(autoreset=True)
 
 merah = Fore.LIGHTRED_EX
 putih = Fore.LIGHTWHITE_EX
@@ -19,182 +22,80 @@ biru = Fore.LIGHTBLUE_EX
 reset = Style.RESET_ALL
 hitam = Fore.LIGHTBLACK_EX
 
-
-class BlumTod:
+class IceBergBot:
     def __init__(self):
         self.base_headers = {
             "accept": "application/json, text/plain, */*",
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0",
             "content-type": "application/json",
-            "origin": "https://telegram.blum.codes",
+            "origin": "https://0xiceberg.store/webapp",
             "x-requested-with": "org.telegram.messenger",
             "sec-fetch-site": "same-site",
             "sec-fetch-mode": "cors",
             "sec-fetch-dest": "empty",
-            "referer": "https://telegram.blum.codes/",
+            "referer": "https://0xiceberg.store/webapp/",
             "accept-encoding": "gzip, deflate",
             "accept-language": "en,en-US;q=0.9",
         }
+
+        import data, proxies
+        self.datas = data.dict_data
+        self.proxies = proxies.dict_proxies
+        self.current_account_proxy = self.proxies[1]
+        self.current_account_number = None
+
+        self.all_end_farming = dict.fromkeys(self.datas, datetime.now(timezone.utc))
+        self.all_balances = dict.fromkeys(self.datas)
+        self.all_claiming_status = dict.fromkeys(self.datas, True)
+        self.config = json.loads(open('config.json', 'r').read())
         self.garis = putih + "~" * 50
 
-    def renew_access_token(self, tg_data):
+    def http(self, url, method = 'post'):
         headers = self.base_headers.copy()
-        data = dp(
-            {
-                "query": tg_data,
-            },
-        )
-        headers["Content-Length"] = str(len(data))
-        url = "https://gateway.blum.codes/v1/auth/provider/PROVIDER_TELEGRAM_MINI_APP"
-        res = self.http(url, headers, data)
-        if "token" not in res.json().keys():
-            self.log(f"{merah}'token' is not found in response, check you data !!")
-            return
+        headers['X-Telegram-Auth'] = self.datas[self.current_account_number]
+        proxy_headers = {
+            'https': self.proxies[self.current_account_number]
+        }
+        matching_methods = {
+            'post': requests.post,
+            'delete': requests.delete,
+            'get': requests.get,
+            'patch': requests.patch
+        }
 
-        access_token = res.json()["token"]["access"]
-        self.log(f"{hijau}success get access token ")
-        return access_token
-
-    def solve_task(self, access_token):
-        url_task = "https://game-domain.blum.codes/api/v1/tasks"
-        headers = self.base_headers.copy()
-        headers["Authorization"] = f"Bearer {access_token}"
-        res = self.http(url_task, headers)
-        for task in res.json():
-            task_id = task["id"]
-            task_title = task["title"]
-            task_status = task["status"]
-            if task_status == "NOT_STARTED":
-                url_start = (
-                    f"https://game-domain.blum.codes/api/v1/tasks/{task_id}/start"
-                )
-                res = self.http(url_start, headers, "")
-                if "message" in res.text:
+        http_func = matching_methods[method]
+        counter_tries = 0
+        while True:
+            try:
+                res = http_func(url=url,
+                                headers=headers,
+                                proxies=proxy_headers,
+                                timeout=30)
+                # res = requests.post(url=url,
+                #                     headers=headers,
+                #                     proxies=proxy_headers,
+                #                     timeout=30)
+                open(".http_request.log", "a", encoding="utf-8").write(res.text + "\n")
+                if "<html>" in res.text:
+                    time.sleep(2)
                     continue
 
-                url_claim = (
-                    f"https://game-domain.blum.codes/api/v1/tasks/{task_id}/claim"
-                )
-                res = self.http(url_claim, headers, "")
-                if "message" in res.text:
-                    continue
+                return res
+            except (
+                    requests.exceptions.ConnectionError,
+                    requests.exceptions.ConnectTimeout,
+                    requests.exceptions.ReadTimeout,
+            ) as e:
+                counter_tries += 1
+                # self.log(f"{merah}connection error, try:{hijau}{counter_tries}")
+                open(".http_request.log", "a", encoding="utf-8").write(repr(e) + "\n")
+                if counter_tries > self.config['max_http_retries']:
+                    self.log(f"{merah}maximum connection retries, raising error")
+                    raise e
 
-                status = res.json()["status"]
-                if status == "CLAIMED":
-                    self.log(f"{hijau}success complete task {task_title} !")
-                    continue
-
-            self.log(f"{kuning}already complete task {task_title} !")
-
-    def claim_farming(self, access_token):
-        url = "https://game-domain.blum.codes/api/v1/farming/claim"
-        headers = self.base_headers.copy()
-        headers["Authorization"] = f"Bearer {access_token}"
-        res = self.http(url, headers, "")
-        balance = res.json()["availableBalance"]
-        self.log(f"{hijau}balance after claim : {balance}")
-        return
-
-    def get_balance(self, access_token, only_show_balance=False):
-        url = "https://game-domain.blum.codes/api/v1/user/balance"
-        headers = self.base_headers.copy()
-        headers["Authorization"] = f"Bearer {access_token}"
-        res = self.http(url, headers)
-        balance = res.json()["availableBalance"]
-        self.log(f"{hijau}balance : {putih}{balance}")
-        if only_show_balance:
-            return
-        timestamp = round(res.json()["timestamp"] / 1000)
-        if "farming" not in res.json().keys():
-            return False, "not_started"
-        end_farming = round(res.json()["farming"]["endTime"] / 1000)
-        if timestamp > end_farming:
-            self.log(f"{hijau}now is time to claim farming !")
-            return True, end_farming
-
-        self.log(f"{kuning}not time to claim farming !")
-        end_date = datetime.fromtimestamp(end_farming)
-        self.log(f'{hijau}end farming : {putih}{end_date}')
-        return False, end_farming
-
-    def start_farming(self, access_token):
-        url = "https://game-domain.blum.codes/api/v1/farming/start"
-        headers = self.base_headers.copy()
-        headers["Authorization"] = f"Bearer {access_token}"
-        res = self.http(url, headers, "")
-        end = res.json()["endTime"]
-        end_date = datetime.fromtimestamp(end / 1000)
-        self.log(f"{hijau}start farming successfully !")
-        self.log(f"{hijau}end farming : {putih}{end_date}")
-        return round(end / 1000)
-
-    def get_friend(self, access_token):
-        url = "https://gateway.blum.codes/v1/friends/balance"
-        headers = self.base_headers.copy()
-        headers["Authorization"] = f"Bearer {access_token}"
-        res = self.http(url, headers)
-        can_claim = res.json()["canClaim"]
-        limit_invite = res.json()["limitInvitation"]
-        amount_claim = res.json()["amountForClaim"]
-        ref_code = res.json()["referralToken"]
-        self.log(f"{putih}limit invitation : {hijau}{limit_invite}")
-        self.log(f"{hijau}claim amount : {putih}{amount_claim}")
-        self.log(f"{putih}can claim : {hijau}{can_claim}")
-        if can_claim:
-            url_claim = "https://gateway.blum.codes/v1/friends/claim"
-            res = self.http(url_claim, headers, "")
-            if "claimBalance" in res.json().keys():
-                self.log(f"{hijau}success claim referral bonus !")
-                return
-            self.log(f"{merah}failed claim referral bonus !")
-            return
-
-    def checkin(self, access_token):
-        url = "https://game-domain.blum.codes/api/v1/daily-reward?offset=-420"
-        headers = self.base_headers.copy()
-        headers["Authorization"] = f"Bearer {access_token}"
-        res = self.http(url, headers)
-        if res.status_code == 404:
-            self.log(f"{kuning}already check in today !")
-            return
-        res = self.http(url, headers, '')
-        if "ok" in res.text.lower():
-            self.log(f'{hijau}success check in today !')
-            return
-
-        self.log(f'{merah}failed check in today !')
-        return
-
-    def playgame(self, access_token):
-        url_play = "https://game-domain.blum.codes/api/v1/game/play"
-        url_claim = "https://game-domain.blum.codes/api/v1/game/claim"
-        url_balance = "https://game-domain.blum.codes/api/v1/user/balance"
-        config = json.loads(open('config.json', 'r').read())
-        game_cfg = config['game_point']
-        low = game_cfg['low']
-        high = game_cfg['high']
-        if low > high:
-            self.log(f'{kuning}high value must be higher than lower value')
-            return
-        headers = self.base_headers.copy()
-        headers['Authorization'] = f"Bearer {access_token}"
-        res = self.http(url_balance, headers)
-        play = res.json()['playPasses']
-        self.log(f'{hijau}you have {putih}{play}{hijau} game ticket')
-        for i in range(play):
-            res = self.http(url_play, headers, '')
-            game_id = res.json()['gameId']
-            self.countdown(30)
-            point = random.randint(low, high)
-            data = json.dumps({"gameId": game_id, "points": point})
-            res = self.http(url_claim, headers, data)
-            if "OK" in res.text:
-                self.log(f'{hijau}success earn {putih}{point}{hijau} from game !')
-                self.get_balance(access_token, only_show_balance=True)
-                continue
-
-            self.log(f'{merah}failed earn {putih}{point}{merah} from game !')
-            continue
+    def log(self, message):
+        now = datetime.now(timezone.utc).isoformat(" ").split(".")[0]
+        print(f"{hitam}[{now}]{reset} {message}")
 
     def data_parsing(self, data):
         redata = {}
@@ -204,50 +105,32 @@ class BlumTod:
 
         return redata
 
-    def log(self, message):
-        now = datetime.now().isoformat(" ").split(".")[0]
-        print(f"{hitam}[{now}]{reset} {message}")
+    def get_balance(self):
+        url_balance = 'https://0xiceberg.store/api/v1/web-app/balance'
+        res = self.http(url=url_balance)
+        self.all_balances[self.current_account_number] = res.json()['amount']
+        self.log(f"{hijau}current balance: {putih}{res.json()['amount']}")
 
-    def http(self, url, headers, data=None):
-        while True:
-            try:
-                if data is None:
-                    headers["content-length"] = "0"
-                    res = requests.get(url, headers=headers, timeout=30)
-                    open(".http_request.log", "a", encoding="utf-8").write(
-                        res.text + "\n"
-                    )
-                    if "<html>" in res.text:
-                        time.sleep(2)
-                        continue
+    def claim_reward(self):
 
-                    return res
-
-                if data == "":
-                    res = requests.post(url, headers=headers, timeout=30)
-                    open(".http_request.log", "a", encoding="utf-8").write(
-                        res.text + "\n"
-                    )
-                    if "<html>" in res.text:
-                        time.sleep(2)
-                        continue
-
-                    return res
-
-                res = requests.post(url, headers=headers, data=data, timeout=30)
-                open(".http_request.log", "a", encoding="utf-8").write(res.text + "\n")
-                if "<html>" in res.text:
-                    time.sleep(2)
-                    continue
-
-                return res
-
-            except (
-                    requests.exceptions.ConnectionError,
-                    requests.exceptions.ConnectTimeout,
-                    requests.exceptions.ReadTimeout,
-            ):
-                self.log(f"{merah}connection error")
+        url_collect = 'https://0xiceberg.store/api/v1/web-app/farming/collect'
+        url_get_next_time = 'https://0xiceberg.store/api/v1/web-app/farming/'
+        get_info = self.http(url_get_next_time, method='post')
+        self.all_end_farming[self.current_account_number] = parser.parse(get_info.json()['stop_time'])
+        time_to_claim = self.all_end_farming[self.current_account_number]
+        if datetime.now(timezone.utc) < time_to_claim:
+            self.log(f"{hijau}not time to claim, it will be after {putih}{time_to_claim.strftime('%d.%m.%y %H:%M')}")
+        else:
+            res = self.http(url=url_collect, method = 'delete')
+            if res.status_code == 201:
+                balance_after_claim = res.json()['amount']
+                self.log(f"{hijau}success to claim reward, balance after claim: {putih}{balance_after_claim}")
+            else:
+                self.log(f"{merah}cannot get reward, maybe it was rewarded ago{merah}")
+            res = self.http(url=url_get_next_time, method = 'post')
+            time_to_claim = parser.parse(res.json()['stop_time'])
+            self.log(f"{hijau} next time to claim will be in {putih}{time_to_claim.strftime('%d.%m.%y %H:%M')}")
+            self.all_end_farming[self.current_account_number] = time_to_claim
 
     def countdown(self, t):
         while t:
@@ -263,10 +146,10 @@ class BlumTod:
 
     def main(self):
         banner = f"""
-    {hijau}AUTO CLAIM FOR {putih}BLUM {hijau}/ {biru}@BlumCryptoBot
+    {hijau}AUTO CLAIM FOR {putih}ICEBERG {hijau}/ {biru}@shishkins
 
-    {hijau}By : {putih}t.me/AkasakaID
-    {putih}Github : {hijau}@AkasakaID
+    {hijau}By : {putih}t.me/tretiakov_aal
+    {putih}Github : {hijau}@shiskins
 
     {hijau}Message : {putih}Dont forget to 'git pull' maybe i update the bot !
         """
@@ -274,46 +157,53 @@ class BlumTod:
         if "noclear" not in arg:
             os.system("cls" if os.name == "nt" else "clear")
         print(banner)
-        datas = open("data.txt", "r").read().splitlines()
-        self.log(f"{hijau}total account : {putih}{len(datas)}")
-        if len(datas) <= 0:
-            self.log(f"{merah}add data account in data.txt")
+        self.log(f"{hijau}total account : {putih}{len(self.datas)}")
+        if len(self.datas) <= 0:
+            self.log(f"{merah}add data account in data.py")
             sys.exit()
 
         self.log(self.garis)
         while True:
-            list_countdown = []
-            for no, data in enumerate(datas):
-                self.log(f"{hijau}account number - {putih}{no + 1}")
-                data_parse = self.data_parsing(data)
+            list_accounts = list(self.datas.keys())
+            np.random.shuffle(list_accounts)
+            for no in list_accounts:
+                self.current_account_number = no
+                self.current_account_proxy = self.proxies[no]
+                data_parse = self.data_parsing(self.datas[no])
                 user = json.loads(data_parse['user'])
+                self.log(f"{hijau}account number - {putih}{no + 1}")
                 self.log(f"{hijau}login as : {putih}{user['first_name']}")
-                access_token = self.renew_access_token(data)
-                self.checkin(access_token)
-                self.get_friend(access_token)
-                self.solve_task(access_token)
-                status, res_bal = self.get_balance(access_token)
-                if status:
-                    self.claim_farming(access_token)
-                    res_bal = self.start_farming(access_token)
-                if isinstance(res_bal, str):
-                    res_bal = self.start_farming(access_token)
-                list_countdown.append(res_bal)
-                self.playgame(access_token)
-                self.log(self.garis)
-                self.countdown(10)
-            min_countdown = min(list_countdown)
-            now = int(time.time())
-            result = min_countdown - now
-            if result <= 0:
+                try:
+                    self.get_balance()
+                    self.claim_reward()
+                except Exception as e:
+                    self.log(f"{merah}something went wrong, continue")
+                    self.log(f"{merah}please check proxy in {putih}{no}{merah}account")
+                    self.all_end_farming[self.current_account_number] += timedelta(1/3)
+
+            list_accounts.sort(key=lambda x: self.all_end_farming[x])
+
+            min_countdown = self.all_end_farming[list_accounts[0]]
+            result = min_countdown - datetime.now(timezone.utc)
+            if result <= timedelta(0):
                 continue
 
-            self.countdown(result)
+            self.countdown(int(result.total_seconds()))
+
 
 
 if __name__ == "__main__":
     try:
-        app = BlumTod()
+        app = IceBergBot()
         app.main()
     except KeyboardInterrupt:
         sys.exit()
+
+# res = requests.post(url, headers, auth = BearerAuth('3pVzwec1Gs1m'))
+
+# IMPORTANT!!
+# это для того, чтобы получить награду
+# requests.delete(url='https://0xiceberg.store/api/v1/web-app/farming/collect/', headers = headers, proxies = proxy_headers).json()
+#
+# это для того, чтобы взять таск
+# requests.patch(url='https://0xiceberg.store/api/v1/web-app/tasks/task/7/', data = headers).text
